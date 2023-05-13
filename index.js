0ae3bb4584df1cab251d38e5cd424525f80638a6
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-13 15:13:18 +0300

    Deploy from render script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-
+app.use(express.static('build'));
 app.use(express.json());
 
 morgan.token('req-body', (req) => {
