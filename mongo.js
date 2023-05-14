512bee0596fb476cc6a34933cc628479860c49f3
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-14 15:59:10 +0300

    3.12 - mongoose connection to db and query/create

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3 || process.argv.length > 5) {
+    console.log('Invalid parameters count');
+}
+
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://admin:${password}@cluster01-fullstackopen.dksmkwb.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url).
+    catch(err => {
+        console.error('Connection to database failed');
+        console.log(err.message);
+        process.exit(1);
+    })
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+
+const Address = mongoose.model('contact', contactSchema);
+
+if(process.argv.length === 5) {
+    addContact();
+} else if(process.argv.length === 3) {
+    getAllContacts();
+}
+
+function addContact() {
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    const newAddress = new Address({name, number});
+    newAddress.save().then(result => {
+        console.log(`Added '${result.name}: ${result.number}' to contacts`);
+        mongoose.connection.close();
+    })
+}
+
+function getAllContacts() {
+    Address.find({}).then(result => {
+        result.forEach(address => {
+            console.log(address);
+        })
+        mongoose.connection.close();
+    })
+}
